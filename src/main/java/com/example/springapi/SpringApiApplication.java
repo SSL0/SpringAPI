152daafff338eafa700de89325885b7e61aa7678
Log1c0 <log1c05678@gmail.com>
2025-01-23 03:49:17 +0700

    Init commit

---
+++ src/main/java/com/example/springapi/SpringApiApplication.java
@@ -0,0 +1,20 @@
+package com.example.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiApplication.class, args);
+    }
+
+    @GetMapping
+    public String HelloWorld(){
+        return "Hello World";
+    }
+}
