b6ec521c8f4b06e8b3ecb144cb613ce736bb0ce9
Log1c0 <log1c05678@gmail.com>
2025-01-23 15:04:49 +0700

    Add group and student inserting to db

---
+++ src/main/java/com/example/springapi/config/Config.java
@@ -0,0 +1,41 @@
+package com.example.springapi.config;
+
+import com.example.springapi.model.Group;
+import com.example.springapi.model.Student;
+import com.example.springapi.repository.GroupRepository;
+import com.example.springapi.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+@Configuration
+public class Config {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository,
+                                        GroupRepository groupRepository) {
+        return args -> {
+            List<Group> groups = List.of(
+                    new Group(null, "8В20", "В", 20)
+            );
+            groupRepository.saveAll(groups);
+
+            List<Student> students = List.of(
+                    new Student(null,
+                            "Ivan",
+                            "Ivan",
+                            "Ivan",
+                            groups.get(0),
+                            new GregorianCalendar(2000, Calendar.MARCH, 25).getTime(),
+                            'M',
+                            "STUDYING"
+                    )
+            );
+            studentRepository.saveAll(students);
+        };
+    }
+
+}
